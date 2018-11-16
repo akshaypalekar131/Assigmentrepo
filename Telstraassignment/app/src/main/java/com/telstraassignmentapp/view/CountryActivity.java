c7141492fdecfed96bdeb578b34dc9e1e9152d98
akshaypalekar131 <44668918+akshaypalekar131@users.noreply.github.com>
2018-11-16 09:40:23 +0530

    R file import added

--- Telstraassignment/app/src/main/java/com/telstraassignmentapp/view/CountryActivity.java
+++ Telstraassignment/app/src/main/java/com/telstraassignmentapp/view/CountryActivity.java
@@ -1,8 +1,10 @@
+
 package com.telstraassignmentapp.view;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import com.telstraassignmentapp.R;
 
 public class CountryActivity extends AppCompatActivity {
 
